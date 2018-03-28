5f1b5eea6d5ab45c70e056085ce82f89fad99ea7
Alex Hicks <alex@alexhicks.net>
2018-03-28 13:57:21 -0400

    Initial (working!) implementation

---
+++ plugin.ts
@@ -0,0 +1,26 @@
+import * as pug from "pug";
+import * as pureta from "pureta";
+
+export default class PugPlugin extends pureta.Plugin {
+    dirs = {};
+
+    async registerHandlers() {
+        this.app.on("server:init", async () => {
+            this.app.server.express.engine("pug", pug.renderFile);
+            this.app.server.express.set("view engine", "pug");
+            // TODO use config for dev mode
+            this.app.server.express.locals.pretty = true;
+            this.app.server.express.disable("view cache");
+        });
+        this.app.on("request:responding", async (handler: pureta.RequestHandler) => {
+            const html = await new Promise((resolve, reject) => {
+                handler.res.render(this.app.views[handler.req.mvcPath], handler.res.locals, (err, html) => {
+                    if (err) reject(err);
+                    else resolve(html);
+                });
+            });
+            handler.res.send(html);
+            handler.res.finished = true;
+        });
+    }
+}
